2059c0726169ea6d8ec59e5a5c989f1bdf9f92f2
Jorge <jorgetrujilloroman@gmail.com>
2026-01-15 16:21:44 +0100

    starting Chanel.cpp

--- src/Channel.cpp
+++ src/Channel.cpp
@@ -1,1 +1,22 @@
-//Channel logic (operators, bans, list of users)
+#include "include/Channel.hpp"
+#include <iostream>
+
+
+Channel::Channel() :
+	_name(""),
+	_userLimit(0),
+	_modeInviteOnly(false),
+	_modeTopicRestricted(true),
+	_hasPassword(false),
+	_hasLimit(false) {}
+
+Channel::Channel(std::string name) :
+	_name(name),
+	_userLimit(0),
+	_modeInviteOnly(false),
+	_modeTopicRestricted(true),
+	_hasPassword(false),
+	_hasLimit(false) {}
+
+Channel::~Channel() {}
+
