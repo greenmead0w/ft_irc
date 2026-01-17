eb872fd65d5f7169db0e564a4a0d8aad01e07aee
Jorge <jorgetrujilloroman@gmail.com>
2026-01-17 20:31:31 +0100

    working on channel

--- src/Channel.cpp
+++ src/Channel.cpp
@@ -20,3 +20,36 @@ Channel::Channel(std::string name) :
 
 Channel::~Channel() {}
 
+Channel::Channel(const Channel &copy) :
+	_name(copy._name),
+	_topic(copy._topic),
+	_password(copy._password),
+	_userLimit(copy._userLimit),
+	_modeInviteOnly(copy._modeInviteOnly),
+	_modeTopicRestricted(copy._modeTopicRestricted),
+	_hasPassword(copy._hasPassword),
+	_hasLimit(copy._hasLimit),
+	_clients(copy._clients),
+	_operators(copy._operators) {}
+
+Channel &Channel::operator=(const Channel &copy) {
+	if (this != &copy) {
+		this->_name = copy._name;
+		this->_topic = copy._topic;
+		this->_password = copy._password;
+		this->_userLimit = copy._userLimit;
+		this->_modeInviteOnly = copy._modeInviteOnly;
+		this->_modeTopicRestricted = copy._modeTopicRestricted;
+		this->_hasPassword = copy._hasPassword;
+		this->_hasLimit = copy._hasLimit;
+		this->_clients = copy._clients;
+		this->_operators = copy._operators;
+	}
+	return *this;
+}
+
+// Management
+
+void	Channel::addClient(Client* client);
+void	Channel::removeClient(int fd);
+bool	Channel::isClientInChannel(int fd) const;
