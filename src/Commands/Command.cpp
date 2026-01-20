b7eb8f192804e6138bbae58cdfaf5bf687a65330
Jorge <jorgetrujilloroman@gmail.com>
2026-01-20 12:03:49 +0100

    channel hpp done. TODO: keep working on cpp

--- src/Commands/Command.cpp
+++ src/Commands/Command.cpp
@@ -1,1 +1,9 @@
-//Base command logic
+#include "Command.hpp"
+
+Command::Command() : 
+	_rawLine(""),
+	_cmdName("") {}
+
+Command::Command(const Command &copy);
+Command::Command &operator=(const Command &copy);
+Command::~Command();
